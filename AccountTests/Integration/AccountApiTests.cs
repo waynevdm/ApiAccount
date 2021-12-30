e851594d53c2a73d1e77998ee5fadaac4b9596dc
Wayne van der Merwe <Wayne.vanderMerwe@sanlam.co.za>
2021-12-30 11:46:50 +0200

    Renamed class and added more asserts

--- AccountTests/Integration/AccountApiTests.cs
+++ AccountTests/Integration/AccountApiTests.cs
@@ -3,22 +3,26 @@ using AccountModel.Api;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace AccountTests.Integration
 {
     [TestClass]
-    public class AccountTests
+    public class AccountApiTests
     {
         [TestMethod]
-        public void AccountApiTests()
+        public void AccountTests()
         {
             // Get All
-            var client = new RestClient("https://localhost:44361/");
+            var client = new RestClient("https://localhost:5001/");
             var getAccountReq = new RestRequest("/Customer");
             var accountResponse = client.Get<IEnumerable<Customer>>(getAccountReq);
             Assert.IsNotNull(accountResponse);
             Assert.AreEqual(accountResponse.StatusCode, HttpStatusCode.OK);
+
+            var customer = accountResponse.Data.FirstOrDefault(x => x.IdNumber == "545464534663");
+            var originalBalance = customer.Accounts[0].Balance;
 
             // Depost
             var depositReq = new RestRequest("/Customer/deposit");
@@ -35,6 +39,9 @@ namespace AccountTests.Integration
             Assert.IsNotNull(depositResponse);
             Assert.AreEqual(depositResponse.StatusCode, HttpStatusCode.OK);
 
+            var balanceAfterDeposit = depositResponse.Data.Accounts[0].Balance;
+            Assert.AreEqual(balanceAfterDeposit, originalBalance + 50);
+
             // Withdraw
             var withdrawReq = new RestRequest("/Customer/withdraw");
             withdrawReq.RequestFormat = DataFormat.Json;
@@ -49,6 +56,7 @@ namespace AccountTests.Integration
             var withdrawResponse = client.Post<Customer>(withdrawReq);
             Assert.IsNotNull(withdrawResponse);
             Assert.AreEqual(withdrawResponse.StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(withdrawResponse.Data.Accounts[0].Balance, balanceAfterDeposit - 30);
         }
     }
 }
